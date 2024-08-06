f42db58454197902f3f5e41ea835d1cff7c17615
Shema Elisa <shemaElyssa@gmail.com>
2024-08-06 20:50:00 +0000

    logic..

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import TimeSetter from "./components/time-setter";
 import Display from "./components/display";
 import AlarmSound from "../src/assets/AlarmSound.mp3";
-import { FaStopwatch,FaPause, FaClock } from "react-icons/fa";
+import { FaStopwatch, FaPause, FaClock } from "react-icons/fa";
 const defaulBreakTime = 5 * 60;
 const defaultSession = 25 * 60;
 const min = 60;
@@ -22,36 +22,59 @@ export default function App() {
 
     }
     function startStop() {
-        console.log("startStop");
+        setDisplayState((prev) => ({
+            ...prev,
+            timerRunning: !prev.timerRunning
+        }))
 
+    }
+    function decrementDisplay() {
+        setDisplayState((prev) => ({
+            ...prev,
+            time: prev.time - 1,
+        }))
+    }
+    function changeBreakTime(time) {
+        if (displayState.timerRunning) return;
+        setBreakTime(time);
+    }
+    function changeSessionTime(time) {
+        if (displayState.timerRunning) return;
+        setSessionTime(time);
+        setDisplayState({
+            time: time,
+            timeType: "session",
+            timerRunning: false
+        })
     }
     return <>
         <div className="container-fluid d-flex justify-content-center">
             <div className="row mt-3 w-50 ">
-                
-                <center  className="p-4"> <h1>CLOCK <FaStopwatch /></h1> </center>
+
+                <center className="p-4"> <h1>CLOCK <FaStopwatch /></h1> </center>
                 <div className="col sm-4 ">
                     <div className="card">
                         <div className="card-header d-flex justify-content-center">
-                           <span className="text-center" style={{fontWeight:500}}> <FaPause/> Break Length</span>  
+                            <span className="text-center" style={{ fontWeight: 500 }}> <FaPause /> Break Length</span>
 
                         </div>
                         <div className="card-body p-3">
-                            <TimeSetter time={breakTime} setTime={setBreakTime} min={min} max={max} interval={interval} type={"break"} />
+                            <TimeSetter time={breakTime} setTime={changeBreakTime} min={min} max={max} interval={interval} type={"break"} />
                         </div>
-                        <div className="p-2">5</div>
+
                     </div>
                 </div>
                 <div className="col sm-4">
                     <div className="card">
                         <div className="card-header d-flex justify-content-center">
 
-                            <span className="text-center" style={{fontWeight:500}}> <FaClock/> Session Length</span>  
+                            <span className="text-center" style={{ fontWeight: 500 }}> <FaClock /> Session Length</span>
                         </div>
                         <div className="card-body d-flex justify-content-center p-3">
-                            <TimeSetter time={sessionTime} setTime={setSessionTime} min={min} max={max} interval={interval} type={"session"} />
+                            <TimeSetter time={sessionTime} setTime={changeSessionTime} min={min} max={max} interval={interval} type={"session"} />
+
                         </div>
-                        <div className="p-2">5</div>
+
                     </div>
                 </div>
                 <div className=" mt-3  d-flex justify-content-center">
--- src/components/time-setter.jsx
+++ src/components/time-setter.jsx
@@ -1,9 +1,9 @@
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
-export default function TimeSetter({time, setTime, min, max, interval, type}) {
+export default function TimeSetter({ time, setTime, min, max, interval, type }) {
 
-    return <div className="d-flex gap-2 w-full">
-        <button className="btn btn-sm btn-primary col-2" onClick={() => (time > min ? setTime(time - interval) : null)}><FaArrowDown /></button>
-        <button  className="btn btn-sm btn-primary col-2" onClick={() => (time < max ? setTime(time + interval) : null)}><FaArrowUp /></button>
+    return <div className="d-flex justify-content-center gap-1 w-full">
+        <button className="btn btn-sm btn-primary " onClick={() => (time > min ? setTime(time - interval) : null)}><FaArrowDown /></button>
+        <button className="btn btn-sm btn-primary " onClick={() => (time < max ? setTime(time + interval) : null)}><FaArrowUp /></button>
     </div>
 }
